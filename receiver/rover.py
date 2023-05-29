a84fd481f83494573f4318958cc7515fc8f965df
justanotherbyte <wishymovies@gmail.com>
2023-05-29 16:33:10 +0100

    rotating lights

--- receiver/rover.py
+++ receiver/rover.py
@@ -1,4 +1,7 @@
-from gpiozero import Motor
+import threading
+import time
+
+from gpiozero import Motor, LED
 
 
 class Rover:
@@ -7,6 +10,8 @@ class Rover:
     MOTOR_RP = 10
     MOTOR_LN = 11
     MOTOR_LP = 8
+
+    LED_PINS = [17, 27, 22, 23]
 
     def __init__(self):
         self.right_motor = Motor(
@@ -22,5 +27,17 @@ class Rover:
             # will always be enabled and disabled at the same time
         )
 
+        self.leds = []
+        for pin_num in self.LED_PINS:
+            self.leds.append(LED(pin_num))
+
+        print(self.leds)
+        threading.Thread(target=self.rotate_lights, daemon=True)
     
 
+    def rotate_lights(self):
+        for led in self.leds:
+            print("led")
+            led.on()
+            time.sleep(0.5)
+            led.off()
