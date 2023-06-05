32a4cdcffe8c716d7c6ed7eecd5d8e9caac83fe8
justanotherbyte <wishymovies@gmail.com>
2023-06-05 20:18:50 +0100

    add read distance method

--- receiver/rover.py
+++ receiver/rover.py
@@ -1,6 +1,6 @@
-import threading
 import time
 
+import RPi.GPIO as GPIO
 from gpiozero import Motor, LED
 
 
@@ -11,9 +11,19 @@ class Rover:
     MOTOR_LN = 11
     MOTOR_LP = 8
 
+    TRIGGER_PIN = 13
+    ECHO_PIN = 25
+
+    SPEED_OF_SOUND_CM_NS = 343 * 100 / 1E9  # 0.0000343 cm / ns
+
     LED_PINS = [17, 27, 22, 23]
 
     def __init__(self):
+        GPIO.setwarnings(False)
+
+        GPIO.setup(self.TRIGGER_PIN, GPIO.OUT)
+        GPIO.setup(self.ECHO_PIN, GPIO.IN)
+
         self.right_motor = Motor(
             forward=self.MOTOR_RP,
             backward=self.MOTOR_RN,
@@ -30,3 +40,23 @@ class Rover:
         self.leds = []
         for pin_num in self.LED_PINS:
             self.leds.append(LED(pin_num))
+
+    def read_distance(self, timeout: int = 50) -> float:
+        offset = 190000
+        GPIO.output(self.TRIGGER_PIN, 1)
+        time.sleep(.00001)  # 10 microseconds
+        GPIO.output(self.ECHO_PIN, 0)
+
+
+        # Wait for the ECHO pin to go high
+        # wait for the pulse rise
+        GPIO.wait_for_edge(self.ECHO_PIN, GPIO.RISING, timeout=timeout)
+        pulse_start = time.perf_counter_ns()
+
+        # And wait for it to fall
+        GPIO.wait_for_edge(self.ECHO_PIN, GPIO.FALLING, timeout=timeout)
+        pulse_end = time.perf_counter_ns()
+
+        pulse_duration = pulse_end - pulse_start - offset
+        distance = (pulse_duration * self.SPEED_OF_SOUND_CM_NS) / 2
+        return max(distance, 0.0)
